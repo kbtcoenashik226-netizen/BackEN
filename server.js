a8b8b0b3d7905689c3aa90afab8411df19859093
Nikhil Ahire <ahirenikhil183@gmail.com>
2025-11-03 14:31:45 +0530

    kar na re plz.........

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use("/images", express.static(path.join(__dirname, "public", "images")));
 
 app.post("/final-report", async (req, res) => {
   try {
